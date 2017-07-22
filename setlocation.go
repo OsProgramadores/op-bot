14de6054936d152957ff13fa8015a761efa32d8b
bcampos@pobox.com <bcampos@pobox.com>
2017-07-22 02:00:44 -0300

    Mais bugs consertados

--- setlocation.go
+++ setlocation.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"errors"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/go-telegram-bot-api/telegram-bot-api"
 	"log"
@@ -39,36 +39,53 @@ func locationHandler(update tgbotapi.Update, bot *tgbotapi.BotAPI, config botCon
 
 	cmd := strings.ToLower(update.Message.Command())
 
+	switch cmd {
+	case "setlocation":
+		if len(args) != 2 {
+			return errors.New("/setlocation <pais> <código postal>")
+		}
+		country := args[0]
+
+		if country != "br" {
+			return fmt.Errorf("Não sei como procurar o Código Postal deste país (%q)", args[0])
+		}
+		cep = args[1]
+	case "cep":
+		if len(args) != 1 {
+			return errors.New("CEP não especificado.")
+		}
+		cep = args[0]
+	}
+
 	if cmd == "setlocation" && len(args) != 2 {
-		return errors.New("/setlocation <pais> <código postal>");
+		return errors.New("/setlocation <pais> <código postal>")
 	}
 
 	if cmd == "cep" && len(args) != 1 {
-		return errors.New("/cep <código postal>");
+		return errors.New("/cep <código postal>")
 	}
 
-	if  (cmd == "setlocation" && args[0] == "br") || cmd == "cep" {
+	if (cmd == "setlocation" && args[0] == "br") || cmd == "cep" {
 		if cmd == "setlocation" {
 			cep = args[1]
 		} else {
 			cep = args[0]
 		}
 
-		if err := findCEP(user,cep,config); err != nil {
-			return errors.New("Não foi possível achar a sua localização . CEP "+cep);
+		if err := findCEP(user, cep, config); err != nil {
+			return fmt.Errorf("Não foi possível achar a sua localização . CEP %q", cep)
 		}
 
-		return errors.New(msgs.LocationSuccess);
+		return errors.New(msgs.LocationSuccess)
 	}
 
-	return errors.New("Não sei como procurar o Código Postal deste país ("+args[0]+")");
+	return fmt.Errorf("Não sei como procurar o Código Postal deste país (%q)", args[0])
 }
 
+// API Key from www.cepaberto.com (brazilian postal code to geo location service.)
 func findCEP(user *tgbotapi.User, cep string, config botConfig) error {
 
-	cepSeguro := url.QueryEscape(cep)
-
-	url := fmt.Sprintf("http://www.cepaberto.com/api/v2/ceps.json?cep=%s", cepSeguro)
+	url := fmt.Sprintf("http://www.cepaberto.com/api/v2/ceps.json?cep=%s", url.QueryEscape(cep))
 
 	req, err := http.NewRequest("GET", url, nil)
 
@@ -87,20 +104,20 @@ func findCEP(user *tgbotapi.User, cep string, config botConfig) error {
 	}
 
 	defer resp.Body.Close()
-	var resultado cepResponse
+	var res cepResponse
 
-	if err := json.NewDecoder(resp.Body).Decode(&resultado); err != nil {
+	if err := json.NewDecoder(resp.Body).Decode(&res); err != nil {
 		return err
 	}
 
-	lat, err := strconv.ParseFloat(resultado.Latitude, 64)
+	lat, err := strconv.ParseFloat(res.Latitude, 64)
 	if err != nil {
-		return fmt.Errorf("Latitude Inválida")
+		return fmt.Errorf("latitude Inválida")
 	}
 
-	long, err := strconv.ParseFloat(resultado.Longitude, 64)
+	long, err := strconv.ParseFloat(res.Longitude, 64)
 	if err != nil {
-		return fmt.Errorf("Loongitude Inválida")
+		return fmt.Errorf("longitude Inválida")
 	}
 
 	err = handleLocation(config.LocationKey, fmt.Sprintf("%d", user.ID), lat, long)
