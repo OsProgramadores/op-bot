f7e6fd7efec56372600a3158462898877aef73c1
Marco Paganini <paganini@paganini.net>
2019-01-01 23:21:46 -0800

    Move bot source to ./src directory.

--- travis-ci/codecheck.sh
+++ travis-ci/codecheck.sh
@@ -12,7 +12,7 @@ trap "rm -f $TMPFILE" EXIT
 
 # Run gofmt in all go files. Report diffs.
 echo "Checking source with gofmt -s."
-find . -type f -name '*.go' -exec gofmt -s -l {} \; >"$TMPFILE"
+find src -type f -name '*.go' -exec gofmt -s -l {} \; >"$TMPFILE"
 
 # Error out if any files need formatting.
 if grep -q . "$TMPFILE"; then
@@ -23,6 +23,7 @@ fi
 
 # From this point on, we exit immediately with any failure.
 set -e
+cd src
 
 echo "Checking source with go-lint."
 golint -set_exit_status
@@ -31,4 +32,5 @@ echo "Checking source with go-vet."
 go vet
 
 echo "Checking translation IDs."
-travis-ci/transcheck/transcheck --source-dir "." --translations-dir "examples/translations"
+cd ..
+travis-ci/transcheck/transcheck --source-dir "src" --translations-dir "examples/translations"
